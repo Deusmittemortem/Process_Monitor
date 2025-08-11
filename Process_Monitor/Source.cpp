2eff7efd1b941b1862d4af4b6111218d4a998a27
Deusmittemortem <deusnosomnes@gmail.com>
2025-08-11 14:36:47 +0300

    Добавьте файлы проекта.

---
+++ Process_Monitor/Source.cpp
@@ -0,0 +1,64 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+
+bool IsProcessRunningByName(const std::wstring& name) {
+    HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (snap == INVALID_HANDLE_VALUE) return false;
+
+    PROCESSENTRY32W pe{};
+    pe.dwSize = sizeof(pe);
+
+    bool running = false;
+    if (Process32FirstW(snap, &pe)) {
+        do {
+            
+            if (_wcsicmp(pe.szExeFile, name.c_str()) == 0) {
+                running = true;
+                break;
+            }
+        } while (Process32NextW(snap, &pe));
+    }
+    CloseHandle(snap);
+    return running;
+}
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    const std::wstring target = L"notepad++.exe"; // имя процесса чувствительно к регистру
+    const auto timeout = std::chrono::seconds(20);
+
+    std::wcout << L"Мониторинг процесса: " << target << L"\n";
+
+    std::chrono::steady_clock::time_point missingSince{};
+    bool wasMissing = false;
+
+    while (true) {
+        bool running = IsProcessRunningByName(target);
+
+        if (running) {
+            wasMissing = false;
+            missingSince = {};
+        }
+        else {
+            if (!wasMissing) {
+                wasMissing = true;
+                missingSince = std::chrono::steady_clock::now();
+            }
+            else {
+                auto absentFor = std::chrono::steady_clock::now() - missingSince;
+                if (absentFor >= timeout) {
+                    //ExitWindowsEx(EWX_LOGOFF, 0);
+                    std::cout << "Процесс выключен\n"; //отладочный код
+                    break;
+                }
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(10)); 
+    }
+    return 0;
+}
